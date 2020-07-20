b194f36f4f9723dafdfacffd54dff8d84f686fd5
kcbaek530 <43196779+kcbaek530@users.noreply.github.com>
2020-07-20 18:44:59 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+  
+   <?php 
+
+   //php 시작 
+
+   echo "Hello World!";
+
+   // php 끝
+   ?>  
+
+</body>
+</html>
